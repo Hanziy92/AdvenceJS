1507a4f5ca209f597d3954d4acc0ff63d65f4f7a
Sofia <hanziypetro@gmail.com>
2022-02-08 20:35:42 +0200

    XMLHttpRequest

--- script.js
+++ script.js
@@ -4,22 +4,59 @@
 // console.log(JSON.stringify(persone)); // ПЕРЕТВОРЮЄ ДЛЯ В ФОРМАТ JSON 
 // console.log(JSON.parse(JSON.stringify(persone))); // pares НАОБОРОТ з JSON 
 
+// const persone = {
+//    name: 'Alex',
+//    tel: '+34444444',
+//    parents: {
+//       mom: 'Olga',
+//       dad: 'Mike'
 
-const persone = {
-   name: 'Alex',
-   tel: '+34444444',
-   parents: {
-      mom: 'Olga',
-      dad: 'Mike'
+//    }
+// };
 
-   }
-};
+// const clone = JSON.parse(JSON.stringify(persone));
+// clone.parents.mom = 'Ann';
 
-const clone = JSON.parse(JSON.stringify(persone));
-clone.parents.mom = 'Ann';
-
-console.log(persone);
-console.log(clone);
+// console.log(persone);
+// console.log(clone);
 
 
+
+// AJAX 
+const inputUah = document.querySelector('#uah'),
+      inputUsd = document.querySelector('#usd');
+
+inputUah.addEventListener('input', () => {
+   const request = new XMLHttpRequest();
+   request.open('GET', '/current.json'); // argument 5 - method, url, async, login, pass.
+   request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+   request.send();
+   // ПОДІЇ
+   // request.addEventListener('readystatechange', () => {
+   //    if (request.readyState === 4 && request.status === 200) {
+   //       console.log(request.response);
+   //       const data = JSON.parse(request.response);
+   //       inputUsd.value = (+inputUah / data.current.usd).toFixed(2);
+   //    } else {
+   //       inputUsd.value = 'Щось пішло не так';
+   //    }
+
+
+      request.addEventListener('load', () => {
+         if (request.status === 200) {
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputUah / data.current.usd).toFixed(2);
+         } else {
+            inputUsd.value = 'Щось пішло не так';
+         }
+
+   });
+   // СВОЙСТВА:
+   // status 
+   // statusText
+   // Response ВІДПОВІДЬ ВІД СЕРВЕРА
+   // readyState 
+
+   
+});
 
