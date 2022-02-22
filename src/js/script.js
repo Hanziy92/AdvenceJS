34de6d0b1771dc0c167964aa1ae24f7d22d667e1
Your Name <you@example.com>
2022-02-22 16:18:30 +0200

    func-generat

--- src/js/script.js
+++ src/js/script.js
@@ -26,23 +26,66 @@
 // import sayHi from './main.js';
 // sayHi();
 
-import $ from 'jquery';
 
-$(document).ready(function() {
-   $('.list-item:first').hover(function() {
-      $(this).toggleClass('active');
-   });
 
-   $('.list-item:eq(2)').on('click', () => {
-      $('.image:even').fadeToggle('slow');
-   });
+// JQUERY
 
-   $('.list-item:eq(4)').on('click', () => {
-      $('.image:odd').animate({
-         opacity: 'toggle',
-         height: 'toggle'
-      }, 2000);
-   });
-});
+
+
+
+
+// import $ from 'jquery';
+
+// $(document).ready(function() {
+//    $('.list-item:first').hover(function() {
+//       $(this).toggleClass('active');
+//    });
+
+//    $('.list-item:eq(2)').on('click', () => {
+//       $('.image:even').fadeToggle('slow');
+//    });
+
+//    $('.list-item:eq(4)').on('click', () => {
+//       $('.image:odd').animate({
+//          opacity: 'toggle',
+//          height: 'toggle'
+//       }, 2000);
+//    });
+// });
+
+
+
+
+// FUNC GENERATOE
+
+// function* generatoe() {
+//    yield 'p';
+//    yield 'e';
+//    yield 't';
+//    yield 'r';
+//    yield 'o';
+// }
+
+// const str = generatoe();
+// // func ВИзиівається кожен раз і робить наступну дію попорядку
+// console.log(str.next().value);
+// console.log(str.next());
+
+// function* count(n) {
+//    for (let i = 0; i < n; i++) {
+//       yield i;
+//    }
+// }
+
+// for (let k of count(7)) {
+//    console.log(k);
+// }
+
+// const counter = count(7);
+
+// console.log(counter.next().value); // 0
+// console.log(counter.next().value); // 1
+// console.log(counter.next().value); // 2
+
 
 
